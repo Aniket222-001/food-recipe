abf503067b728b5fc1e971768c55fa4fdbeeb69a
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-07-03 00:13:17 +0530

    Update Helpus.js

--- src/components/Helpus.js
+++ src/components/Helpus.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Help = () => {
   return (
     <div className='text-bold text-[40px] text-orange-700 flex justify-center items-center h-screen'>
-     Sorry out of service
+     under maintenance
     </div>
   )
 }
