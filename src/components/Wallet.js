3785badd8f53f9487b4f548a15930456662e5d54
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-07-03 00:12:48 +0530

    Update Wallet.js

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Wallet = () => {
   return (
     <div className='text-bold text-[40px] text-orange-700 flex justify-center items-center h-screen'>
-     No Money
+     under maintenance
     </div>
   )
 }
