c1be613642e9ffa8f26c5f0a2a004aa269007e52
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-07-03 00:11:52 +0530

    Update Myfav.js

--- src/components/Myfav.js
+++ src/components/Myfav.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Myfav = () => {
   return (
     <div className='text-bold text-[40px] text-orange-700 flex justify-center items-center h-screen'>
-     why are u interseted in myfav
+     Empty
     </div>
   )
 }
